3d31f24c40037726fabf5499de55ae3ad3032c4d
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-01 14:52:35 +0100

    created sign up form component

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,13 +1,78 @@
-import React, { UseState } from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+// TODO - import auth here
+
+// mutations/queries
+import { ADD_USER } from "../utils/mutations";
 
 function SignUpForm() {
+  // set the initial form state
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // set state for form validation
+  const [validated] = useState(false);
+
+  //   add user mutation
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  //   handle input change
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  //   handle form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      //   add authentication here
+    } catch (error) {
+      console.error(error);
+    }
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
+    console.log(userFormData);
+  };
+
   return (
     <>
-    {/* creating a user */}
-      <form>
-        <input placeholder="username"></input>
-        <input placeholder="email"></input>
-        <input placeholder="password" type="password"></input>
+      {/* creating a user */}
+      <form onSubmit={handleFormSubmit}>
+        {/* username */}
+        <input
+          value={userFormData.username}
+          placeholder="username"
+          name="username"
+          onChange={handleInputChange}
+          required
+        ></input>
+        {/* email */}
+        <input
+          value={userFormData.email}
+          placeholder="email"
+          name="email"
+          onChange={handleInputChange}
+          required
+        ></input>
+        {/* password */}
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          onChange={handleInputChange}
+          required
+        ></input>
+        <button type="submit">SUBMIT</button>
       </form>
     </>
   );
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+// sign up user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      #   token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
