5e5578ba553d5aab5c2636622a67b1522841f821
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-03 22:05:16 +0100

    created chat data file for dev purposes

---
+++ server/data/data.js
@@ -0,0 +1,49 @@
+const chatData = [
+  {
+    //   users within the chat
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Bav Kudhail",
+        email: "bavkudhail@example.com",
+      },
+      {
+        name: "Johnny Bravo",
+        email: "john@example.com",
+      },
+    ],
+    // chat information
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      // the name of the company / user that created the chat
+      name: "Company Name",
+    },
+  },
+  {
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Laura W",
+        email: "lauraw@example.com",
+      },
+      {
+        name: "Grant T",
+        email: "grantt@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Project 3",
+    groupAdmin: {
+      name: "Guest User",
+    },
+  },
+];
+
+module.exports = chatData;
