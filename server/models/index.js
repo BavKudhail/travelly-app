5c6cc7d56f65c9713c8a7eda3b886ee5930ea32c
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-03 22:04:26 +0100

    added chat / message to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,27 @@
-const User = require('./User');
-const Trip = require('./Trip');
-const Activity = require('./Activity');
-const ActivityBadge = require('./ActivityBadge');
-const Country = require('./Country');
-const CountryBadge = require('./CountryBadge');
-const Company = require('./Company');
-const Admin = require('./Admin');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Trip = require("./Trip");
+const Activity = require("./Activity");
+const ActivityBadge = require("./ActivityBadge");
+const Country = require("./Country");
+const CountryBadge = require("./CountryBadge");
+const Company = require("./Company");
+const Admin = require("./Admin");
+const Post = require("./Post");
+const Comment = require("./Comment");
+const Chat = require("./Chat");
+const Message = require("./Message");
 
-module.exports = { User, Trip, Activity, ActivityBadge, Country, CountryBadge, Company, Admin, Post, Comment };
+module.exports = {
+  User,
+  Trip,
+  Activity,
+  ActivityBadge,
+  Country,
+  CountryBadge,
+  Company,
+  Admin,
+  Post,
+  Chat,
+  Message,
+  Comment,
+};
