981ce165ef061e53cb00ed17edb3b317d5c87f42
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-08 20:05:30 +0100

    added navbar to app.js and created styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 // dependencies
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 // router
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // provider
 import ChatProvider from "./context/ChatProvider";
-
+// chakra/ui / styling
+import { Flex } from "@chakra-ui/react";
+import backgroundimg from "./assets/background.png";
 // pages
-import Home from './pages/Home';
-import { Chat, Landing, Dashboard, Posts } from './pages';
-
+import Home from "./pages/Home";
+import { Chat, Landing, Dashboard, Posts } from "./pages";
 // components
-import { Navbar } from './components';
+import { NavBar } from "./components";
 
 // http link
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
+
 
 // auth link
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       // we are setting the header on every network request that we make to have the auth token that is available
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -48,15 +52,22 @@ function App() {
     <ApolloProvider client={client}>
       <ChatProvider>
         <Router>
-          <div className="">
-            {/* <Navbar /> */}
+          <Flex
+            h="100vh"
+            maxW="2000px"
+            flexDir="row"
+            overflow="hidden"
+            backgroundImage={backgroundimg}
+            className="app"
+          >
+            <NavBar />
             <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/chat" element={<Chat />} />
+              <Route path="/" element={<Landing />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/posts" element={<Posts />} />
+              <Route path="/chat" element={<Chat />} />
             </Routes>
-          </div>
+          </Flex>
         </Router>
       </ChatProvider>
     </ApolloProvider>
