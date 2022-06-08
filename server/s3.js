b3d0154d61ecde462a2681fe36936722947216ca
Laura Walters <laura.walters1098@yahoo.com>
2022-06-08 22:23:57 +0100

    removing .env values

--- server/s3.js
+++ server/s3.js
@@ -3,11 +3,10 @@ const fs = require("fs");
 
 const S3 = require("aws-sdk/clients/s3");
 
-const bucketName = (process.env.AWS_BUCKET_NAME = "travelly");
-const region = (process.env.AWS_BUCKET_REGION = "eu-west-2");
-const accessKeyId = (process.env.AWS_ACCESS_KEY = "AKIA3BHCHAE5Z2X44L6W");
-const secretAccessKey = (process.env.AWS_SECRET_KEY =
-  "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");
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
   region,
