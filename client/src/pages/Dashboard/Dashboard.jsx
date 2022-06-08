9fd34a9cb5a63a40f566c194b277afe888d089ad
GrantRT <granto_92@hotmail.com>
2022-06-08 15:45:41 +0100

    added my posts to dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { useMutation, useQuery } from "@apollo/react-hooks";
-import DashboardRight from "../../components/Dashboard/DashboardRight";
+import React from 'react';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+import DashboardRight from '../../components/Dashboard/DashboardRight';
 
 // mutations/queries
-import { GET_DASHBOARD } from "../../utils/queries";
+import { GET_DASHBOARD } from '../../utils/queries';
 
 const Dashboard = () => {
-  const staticUserId = "6299eaa2b3b3eb625a753dd0";
+  const staticUserId = '6299eaa2b3b3eb625a753dd0';
   // Execute the query on component load
   const { loading, data, error } = useQuery(GET_DASHBOARD, {
     variables: {
@@ -17,8 +17,7 @@ const Dashboard = () => {
 
   console.log(userData);
 
-//   
-
+  //
 
   return (
     <>
@@ -31,6 +30,9 @@ const Dashboard = () => {
           {/* MAIN */}
           <div className="main-section">
             <h1>MAIN SECTION</h1>
+            <div> USERNAME: {userData.username}</div>
+            <div> FOLLOWER: {userData.followerCount}</div>
+            <div> FOLLOWING: {userData.followingCount}</div>
           </div>
           {/* RIGHT */}
           <DashboardRight />
@@ -42,7 +44,10 @@ const Dashboard = () => {
             })}
           </div>
           {/* MY POSTS */}
-          <h1>MY TRIPS</h1>
+          <h1>MY POSTS</h1>
+          {userData.posts.map((post) => {
+            return <div key={post._id}>{post.postText}</div>;
+          })}
         </div>
       )}
     </>
