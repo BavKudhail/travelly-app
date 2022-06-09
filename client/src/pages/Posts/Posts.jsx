73f47bfacb9ca7ddf2618d9755dd58cdbc0e5d35
GrantRT <granto_92@hotmail.com>
2022-06-09 15:04:43 +0100

    can post to db on frontend

--- client/src/pages/Posts/Posts.jsx
+++ client/src/pages/Posts/Posts.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 
 import { GET_POSTS } from '../../utils/queries';
+import { ADD_POST } from '../../utils/mutations';
+
+// static user for testing - this needs to come from the loggedin user
+const user = {
+  _id: '6299eaa2b3b3eb625a753dd0',
+  username: 'Max Kanat-Alexander',
+  email: 'mkanatalexander@techfriends.dev',
+};
 
 const Posts = () => {
   // Execute the query on component load
   const { loading, data, error } = useQuery(GET_POSTS, {});
 
-  console.log(data);
-
   const postData = data || [];
 
-  console.log(postData);
+  const [addPost] = useMutation(ADD_POST);
+
+  const [postText, setPostText] = useState('');
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const { data } = await addPost({
+      variables: {
+        userId: user._id,
+        postText: postText,
+      },
+    });
+  };
 
   //
 
@@ -26,6 +44,15 @@ const Posts = () => {
           {/* MAIN */}
           <div className="main-section">
             <h1>MAIN SECTION</h1>
+          </div>
+          <div>
+            <h1>ADD POST</h1>
+            <form>
+              <textarea name="postTextArea" type="text" value={postText} onChange={(e) => setPostText(e.target.value)}></textarea>
+              <button type="submit" onClick={handleFormSubmit}>
+                Submit
+              </button>
+            </form>
           </div>
           {/* RIGHT */}
           <div>
