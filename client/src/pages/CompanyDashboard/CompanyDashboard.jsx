76f535ef7e0be5390dd315f6f2805753b6a9f2c7
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-13 17:20:33 +0100

    created "createTrip" modal

--- client/src/pages/CompanyDashboard/CompanyDashboard.jsx
+++ client/src/pages/CompanyDashboard/CompanyDashboard.jsx
@@ -2,29 +2,36 @@ import React from "react";
 import { useMutation, useQuery } from "@apollo/react-hooks";
 // mutations/queries
 import { COMPANY_DASHBOARD } from "../../utils/queries";
-import CompanyTripCard from "../../components/Trip/CompanyTripCard"
-
-
-
+import CompanyTripCard from "../../components/Trip/CompanyTripCard";
 import { CreateTrip } from "../../components/";
+import { Flex } from "@chakra-ui/react";
 
 function CompanyDashboard() {
-  const {data, error, loading} = useQuery(COMPANY_DASHBOARD)
+  const { data, error, loading } = useQuery(COMPANY_DASHBOARD);
   const tripData = data?.getCompanyTrips || [];
 
-  return (<>
-  <CreateTrip />;
-  {/* <CompanyTripCard /> */}
-
-  {tripData.map((trip)=>{
-
-    return <CompanyTripCard key={trip._id} tripId={trip._id} tripName={trip.tripName} tripDescription={trip.tripDescription} startDate={trip.startDate} countries={trip.countries} activities={trip.activities}  endDate={trip.endDate} image={trip.imageUrl}/>
-  })}
-
-
-
-  </>)
-
+  return (
+    <>
+      <Flex mt="10" justifyContent={"center"} w="100%">
+        <CreateTrip />
+      </Flex>
+      {tripData.map((trip) => {
+        return (
+          <CompanyTripCard
+            key={trip._id}
+            tripId={trip._id}
+            tripName={trip.tripName}
+            tripDescription={trip.tripDescription}
+            startDate={trip.startDate}
+            countries={trip.countries}
+            activities={trip.activities}
+            endDate={trip.endDate}
+            image={trip.imageUrl}
+          />
+        );
+      })}
+    </>
+  );
 }
 
 export default CompanyDashboard;
