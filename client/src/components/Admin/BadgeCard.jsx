284fc2ef20ab889409fd1d510c60c1f42eafeed8
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-06-13 17:34:19 +0100

    refactored admin dashboard UI

--- client/src/components/Admin/BadgeCard.jsx
+++ client/src/components/Admin/BadgeCard.jsx
@@ -2,33 +2,32 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import { MotionConfig } from "framer-motion";
-import BadgeImageForm from  "./BadgeImageForm"
+import BadgeImageForm from "./BadgeImageForm";
 
+import {
+  Box,
+  Image,
+  Badge,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
-import { Box, Image, Badge, Button, useDisclosure, Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton, } from "@chakra-ui/react";
-
-const BadgeCard = ({
-  badgeName,
-  badgeImage,
-  array,
-  model,
-  badgeId
-  
-}) => {
+const BadgeCard = ({ badgeName, badgeImage, array, model, badgeId }) => {
   const property = {
     imageUrl:
       "https://static.seeker.io/media/img/1/9f2de8cd-b9b1-4165-a23d-39353fb68f93-1400.jpg?placeid=6342&name=Lavender%20Field,%20Valensole&lat=43.833843&lng=6.026507",
     imageAlt: "Rear view of modern home with pool",
   };
-  console.log(badgeImage)
+  console.log(badgeImage);
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box
@@ -38,12 +37,9 @@ const BadgeCard = ({
       overflow="hidden"
       boxShadow={"2xl"}
       my="10"
+      p="30px"
     >
-      <Image
-        src={badgeImage}
-        alt={property.imageAlt}
-        borderRadius="30px"
-      />
+      <Image src={badgeImage} alt={property.imageAlt} borderRadius="30px" />
 
       <Box p="6">
         <Box display="flex" alignItems="baseline">
@@ -60,26 +56,28 @@ const BadgeCard = ({
         >
           {badgeName}
         </Box>
-       
+
         <Button onClick={onOpen}>Upload image</Button>
- <Modal isOpen={isOpen} onClose={onClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>{badgeName}</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <BadgeImageForm badgeName={badgeName} badgeId={badgeId} model={model} />
-        
-   
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{badgeName}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <BadgeImageForm
+                badgeName={badgeName}
+                badgeId={badgeId}
+                model={model}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </Box>
   );
--- client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -3,138 +3,174 @@ import { useMutation, useQuery } from "@apollo/react-hooks";
 // mutations/queries
 import { ADMIN_DASHBOARD } from "../../utils/queries";
 
-import CountryForm from "../../components/Admin/CountryForm"
-import ActivityForm from "../../components/Admin/ActivityForm"
-import CountryBadgeForm from "../../components/Admin/CountryBadgeForm"
-import ActivityBadgeForm from "../../components/Admin/ActivityBadgeForm"
-import BadgeCard from "../../components/Admin/BadgeCard"
+import CountryForm from "../../components/Admin/CountryForm";
+import ActivityForm from "../../components/Admin/ActivityForm";
+import CountryBadgeForm from "../../components/Admin/CountryBadgeForm";
+import ActivityBadgeForm from "../../components/Admin/ActivityBadgeForm";
+import BadgeCard from "../../components/Admin/BadgeCard";
 
-import { Box, Image, Badge, Button, useDisclosure, Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton, } from "@chakra-ui/react";
-
-
-
-
-
-
-
+import {
+  Box,
+  Image,
+  Badge,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Flex,
+} from "@chakra-ui/react";
 
 function AdminDashboard() {
-  const {data, error, loading} = useQuery(ADMIN_DASHBOARD)
+  const { data, error, loading } = useQuery(ADMIN_DASHBOARD);
 
-  const countryBadges = data?.getAllCountryBadges
-  const activityBadges = data?.getAllActivityBadges
+  const countryBadges = data?.getAllCountryBadges;
+  const activityBadges = data?.getAllActivityBadges;
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const { isOpen: isActivityOpen, onOpen: onActivityOpen, onClose: onActivityClose } = useDisclosure()
-  const { isOpen: isCountryBadgeOpen, onOpen: onCountryBadgeOpen, onClose: onCountryBadgeClose } = useDisclosure()
-  const { isOpen: isActivityBadgeOpen, onOpen: onActivityBadgeOpen, onClose: onActivityBadgeClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isActivityOpen,
+    onOpen: onActivityOpen,
+    onClose: onActivityClose,
+  } = useDisclosure();
+  const {
+    isOpen: isCountryBadgeOpen,
+    onOpen: onCountryBadgeOpen,
+    onClose: onCountryBadgeClose,
+  } = useDisclosure();
+  const {
+    isOpen: isActivityBadgeOpen,
+    onOpen: onActivityBadgeOpen,
+    onClose: onActivityBadgeClose,
+  } = useDisclosure();
 
-      console.log(data)
-  
-if(!loading){
-  return (<>
- <h1>Admin dashboard</h1>
- {/* COUNTRY MODAL */}
- <Button onClick={onOpen}>Add country</Button>
- <Modal isOpen={isOpen} onClose={onClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Modal Title</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <CountryForm />
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
+  console.log(data);
 
-  <Button onClick={onActivityOpen}>Add activity</Button>
- <Modal isOpen={isActivityOpen} onClose={onActivityClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Modal Title</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <ActivityForm />
-        
-   
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onActivityClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
+  if (!loading) {
+    return (
+      <>
+        <Flex flexDir={"column"} w="100%" overflow={"auto"}>
+          <Flex mt="10" justifyContent={"center"} w="100%">
+            <Button onClick={onOpen}>Add country</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <CountryForm />
+                </ModalBody>
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
 
+            <Button onClick={onActivityOpen}>Add activity</Button>
+            <Modal isOpen={isActivityOpen} onClose={onActivityClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <ActivityForm />
+                </ModalBody>
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onActivityClose}>
+                    Close
+                  </Button>
+                  <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
 
-  <Button onClick={onCountryBadgeOpen}>Add Country Badge</Button>
- <Modal isOpen={isCountryBadgeOpen} onClose={onCountryBadgeClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Modal Title</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <CountryBadgeForm />
-        
-   
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onCountryBadgeClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
+            <Button onClick={onCountryBadgeOpen}>Add Country Badge</Button>
+            <Modal isOpen={isCountryBadgeOpen} onClose={onCountryBadgeClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <CountryBadgeForm />
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    colorScheme="blue"
+                    mr={3}
+                    onClick={onCountryBadgeClose}
+                  >
+                    Close
+                  </Button>
+                  <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
 
-  <Button onClick={onActivityBadgeOpen}>Add Activity Badge</Button>
- <Modal isOpen={isActivityBadgeOpen} onClose={onActivityBadgeClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Modal Title</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <ActivityBadgeForm />
-        
-   
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onActivityBadgeClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-
-<div className="d-flex flex-column"> 
-  {countryBadges.map((badge)=>{
-    return <BadgeCard key={badge._id} badgeName={badge.badgeName} badgeId={badge._id} model="CountryBadge" badgeImage={badge.badgeImage}/>
-  })}
-
-</div>
-
-<div className="d-flex flex-column"> 
-{activityBadges.map((badge)=>{
-    return <BadgeCard key={badge._id} badgeName={badge.badgeName} badgeId={badge._id} model="ActivityBadge" badgeImage={badge.badgeImage}/>
-  })}
-  </div>
-  </>)
-}
-
+            <Button onClick={onActivityBadgeOpen}>Add Activity Badge</Button>
+            <Modal isOpen={isActivityBadgeOpen} onClose={onActivityBadgeClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <ActivityBadgeForm />
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    colorScheme="blue"
+                    mr={3}
+                    onClick={onActivityBadgeClose}
+                  >
+                    Close
+                  </Button>
+                  <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Flex>
+          <Flex w="100%" justifyContent={"center"}>
+            <Flex mx="20px">
+              <Flex flexDir={"column"}>
+                {countryBadges.map((badge) => {
+                  return (
+                    <BadgeCard
+                      key={badge._id}
+                      badgeName={badge.badgeName}
+                      badgeId={badge._id}
+                      model="CountryBadge"
+                      badgeImage={badge.badgeImage}
+                    />
+                  );
+                })}
+              </Flex>
+            </Flex>
+            <Flex mx="20px">
+              <Flex flexDir={"column"}>
+                {activityBadges.map((badge) => {
+                  return (
+                    <BadgeCard
+                      key={badge._id}
+                      badgeName={badge.badgeName}
+                      badgeId={badge._id}
+                      model="ActivityBadge"
+                      badgeImage={badge.badgeImage}
+                    />
+                  );
+                })}
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+      </>
+    );
+  }
 }
 
 export default AdminDashboard;
