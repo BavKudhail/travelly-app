f6c2c10ac02f79f0b69f6dda1e218e712c59533a
GrantRT <granto_92@hotmail.com>
2022-06-16 14:28:43 +0100

    styling for user login page

--- client/src/pages/UserLogin/UserLogin.jsx
+++ client/src/pages/UserLogin/UserLogin.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import UserSignUpForm from '../../components/User/UserSignupForm';
-import UserLoginForm from '../../components/User/UserLoginForm';
+import SignUpForm from '../../components/SignUpForm';
+import LoginForm from '../../components/LoginForm';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Flex } from '@chakra-ui/react';
 
 const UserLogin = () => {
   return (
     <>
-      <UserSignUpForm />
-      <UserLoginForm />
+      <Flex width="100%" justifyContent={'center'} alignItems={'center'}>
+        <Tabs variant="soft-rounded" colorScheme="purple" className="glassmorphism" p="20">
+          <TabList>
+            <Tab>Login</Tab>
+            <Tab>Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <LoginForm />
+            </TabPanel>
+            <TabPanel>
+              <SignUpForm />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
     </>
   );
 };
